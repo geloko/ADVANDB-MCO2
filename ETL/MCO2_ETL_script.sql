387aebc7104a0bc3b1a27f8ffacf809b23aae5e0
geloko <angelo92497@yahoo.com>
2016-03-19 21:15:19 +0800

    changes in the schema

--- ETL/MCO2_ETL_script.sql
+++ ETL/MCO2_ETL_script.sql
@@ -3,9 +3,9 @@
 SELECT distinct mun, zone, brgy, purok FROM db_hpq.hpq_hh;
 
 -- Load crop
-#INSERT INTO crop(volume, line, typeID)
-SELECT distinct crop_vol, crop_line, croptype FROM db_hpq.hpq_crop;
+#INSERT INTO crop(volume, line, typeID, householdID)
+SELECT distinct crop_vol, crop_line, croptype, h.id FROM db_hpq.hpq_crop c INNER JOIN db_hpq.hpq_hh h ON( c.hpq_hh_id = h.id);
 
 -- Load aquani
-#INSERT INTO aquani(volume, line, typeID)
-SELECT distinct aquani_vol, aquani_line, aquanitype FROM db_hpq.hpq_aquani;
+#INSERT INTO aquani(volume, line, typeID, householdID)
+SELECT distinct aquani_vol, aquani_line, aquanitype, h.id FROM db_hpq.hpq_aquani a INNER JOIN db_hpq.hpq_hh h ON( a.hpq_hh_id = h.id);
