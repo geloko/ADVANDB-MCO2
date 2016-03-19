89a2e070ca7458e6c6ecb37cd0518fb5272e8382
geloko <angelo92497@yahoo.com>
2016-03-19 21:22:30 +0800

    hardcoded calamity type values

--- ETL/MCO2_ETL_script.sql
+++ ETL/MCO2_ETL_script.sql
@@ -9,3 +9,15 @@ SELECT distinct crop_vol, crop_line, croptype, h.id FROM db_hpq.hpq_crop c INNER
 -- Load aquani
 #INSERT INTO aquani(volume, line, typeID, householdID)
 SELECT distinct aquani_vol, aquani_line, aquanitype, h.id FROM db_hpq.hpq_aquani a INNER JOIN db_hpq.hpq_hh h ON( a.hpq_hh_id = h.id);
+
+-- Load Calamity type
+INSERT INTO calamity_type VALUES (1, "typhoon");
+INSERT INTO calamity_type VALUES (2, "flood");
+INSERT INTO calamity_type VALUES (3, "drought");
+INSERT INTO calamity_type VALUES (4, "earthquake");
+INSERT INTO calamity_type VALUES (5, "volcanic eruption");
+INSERT INTO calamity_type VALUES (6, "landslide");
+INSERT INTO calamity_type VALUES (7, "tsunami");
+INSERT INTO calamity_type VALUES (8, "fire");
+INSERT INTO calamity_type VALUES (9, "forest fire");
+ 
