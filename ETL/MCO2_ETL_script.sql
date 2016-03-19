ca606837d7dda9abb2c0cf50aa57d432208409b8
geloko <angelo92497@yahoo.com>
2016-03-19 19:59:02 +0800

    sql script changes

--- ETL/MCO2_ETL_script.sql
+++ ETL/MCO2_ETL_script.sql
@@ -4,8 +4,8 @@ SELECT distinct mun, zone, brgy, purok FROM db_hpq.hpq_hh;
 
 -- Load crop
 #INSERT INTO crop(volume, line, typeID)
-SELECT distinct crop_vol, crop_line, croptype FROM db_hpq.hpq_hh h INNER JOIN dp_hpq.hpq_crop c ON (h.id = c.hpq_hh_id);
+SELECT distinct crop_vol, crop_line, croptype FROM db_hpq.hpq_crop;
 
 -- Load aquani
 #INSERT INTO aquani(volume, line, typeID)
-SELECT distinct aquani_vol, aquani_line, aquanitype FROM db_hpq.hpq_hh h INNER JOIN dp_hpq.hpq_aquani a ON (h.id = a.hpq_hh_id);
+SELECT distinct aquani_vol, aquani_line, aquanitype FROM db_hpq.hpq_aquani;
