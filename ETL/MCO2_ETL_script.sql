f723defcc92a87149bec0adc506f3bf00b65522e
geloko <angelo92497@yahoo.com>
2016-03-23 12:32:56 +0800

    ETL

--- ETL/MCO2_ETL_script.sql
+++ ETL/MCO2_ETL_script.sql
@@ -1,14 +1,6 @@
 -- Load location
-#INSERT INTO location(mun, zone, brgy, purok)
+INSERT INTO location(mun, zone, brgy, purok)
 SELECT distinct mun, zone, brgy, purok FROM db_hpq.hpq_hh;
-
--- Load crop
-#INSERT INTO crop(volume, line, typeID, householdID)
-SELECT distinct crop_vol, crop_line, croptype, h.id FROM db_hpq.hpq_crop c INNER JOIN db_hpq.hpq_hh h ON( c.hpq_hh_id = h.id);
-
--- Load aquani
-#INSERT INTO aquani(volume, line, typeID, householdID)
-SELECT distinct aquani_vol, aquani_line, aquanitype, h.id FROM db_hpq.hpq_aquani a INNER JOIN db_hpq.hpq_hh h ON( a.hpq_hh_id = h.id);
 
 -- Load Calamity type
 INSERT INTO calamity_type VALUES (1, "typhoon");
@@ -21,28 +13,6 @@ INSERT INTO calamity_type VALUES (7, "tsunami");
 INSERT INTO calamity_type VALUES (8, "fire");
 INSERT INTO calamity_type VALUES (9, "forest fire");
 
-/*
--- Load calamity
-INSERT INTO calamity(freq_year)
-SELECT 1 as typeID, AVG(calam1_hwmny) as freq_year FROM db_hpq.hpq_hh GROUP BY mun, zone, brgy, purok;
-INSERT INTO calamity(freq_year)
-SELECT 2 as typeID, AVG(calam2_hwmny) as freq_year FROM db_hpq.hpq_hh GROUP BY mun, zone, brgy, purok;
-INSERT INTO calamity(freq_year)
-SELECT 3 as typeID, AVG(calam3_hwmny) as freq_year FROM db_hpq.hpq_hh GROUP BY mun, zone, brgy, purok;
-INSERT INTO calamity(freq_year)
-SELECT 4 as typeID, AVG(calam4_hwmny) as freq_year FROM db_hpq.hpq_hh GROUP BY mun, zone, brgy, purok;
-INSERT INTO calamity(freq_year)
-SELECT 5 as typeID, AVG(calam5_hwmny) as freq_year FROM db_hpq.hpq_hh GROUP BY mun, zone, brgy, purok;
-INSERT INTO calamity(freq_year)
-SELECT 6 as typeID, AVG(calam6_hwmny) as freq_year FROM db_hpq.hpq_hh GROUP BY mun, zone, brgy, purok;
-INSERT INTO calamity(freq_year)
-SELECT 7 as typeID, AVG(calam7_hwmny) as freq_year FROM db_hpq.hpq_hh GROUP BY mun, zone, brgy, purok;
-INSERT INTO calamity(freq_year)
-SELECT 8 as typeID, AVG(calam8_hwmny) as freq_year FROM db_hpq.hpq_hh GROUP BY mun, zone, brgy, purok;
-INSERT INTO calamity(freq_year)
-SELECT 9 as typeID, AVG(calam9_hwmny), mun, zone, brgy, purok FROM db_hpq.hpq_hh GROUP BY mun, zone, brgy, purok;
-*/ 
-
 -- load aquani type
 INSERT INTO aquani_type(type) VALUES("Tilapia");
 INSERT INTO aquani_type(type) VALUES("Milkfish");
@@ -50,6 +20,7 @@ INSERT INTO aquani_type(type) VALUES("Catfish");
 INSERT INTO aquani_type(type) VALUES("Mudfish");
 INSERT INTO aquani_type(type) VALUES("Carp");
 INSERT INTO aquani_type(type) VALUES("Others");
+INSERT INTO aquani_type(type) VALUES("Undefined");
 
 -- load crop type
 INSERT INTO crop_type(type) VALUES("Sugar Cane");
@@ -57,27 +28,50 @@ INSERT INTO crop_type(type) VALUES("Palay");
 INSERT INTO crop_type(type) VALUES("Corn");
 INSERT INTO crop_type(type) VALUES("Coffee");
 INSERT INTO crop_type(type) VALUES("Other Crops");
+INSERT INTO crop_type(type) VALUES("Undefined");
 
-INSERT INTO house(housetype, wall_mat, roof_mat)
-SELECT distinct (CASE WHEN house_type = 1 THEN "Single House"
+
+INSERT INTO housetype
+SELECT distinct house_type, (CASE WHEN house_type = 1 THEN "Single House"
 			 WHEN house_type = 2 THEN "Duplex"
 			 WHEN house_type = 3 THEN "Multi-Unit Residential"
 			 WHEN house_type = 4 THEN "Commercial/Agricultural/Industrial"
-		END) as housetype,
-		(CASE WHEN wall = 1 THEN "Strong Materials"
+			 WHEN house_type = 5 THEN "Others"
+		END)
+FROM db_hpq.hpq_hh;
+
+INSERT INTO wall
+SELECT distinct wall, (CASE WHEN wall = 1 THEN "Strong Materials"
 			  WHEN wall = 2 THEN "Light Materials"
 			  WHEN wall = 3 THEN "Makeshift Materials"
 			  WHEN wall = 4 THEN "Predominantly Strong"
 			  WHEN wall = 5 THEN "Predominantly Light"
 			  WHEN wall = 6 THEN "Predominantly Makeshift"
 			  WHEN wall = 7 THEN "N/A"		
-		END) as wall_mat,
-		(CASE WHEN roof = 1 THEN "Strong Materials"
+		END)
+FROM db_hpq.hpq_hh;
+
+INSERT INTO roof
+SELECT distinct roof, (CASE WHEN roof = 1 THEN "Strong Materials"
 			  WHEN roof = 2 THEN "Light Materials"
 			  WHEN roof = 3 THEN "Makeshift Materials"
 			  WHEN roof = 4 THEN "Predominantly Strong"
 			  WHEN roof = 5 THEN "Predominantly Light"
 			  WHEN roof = 6 THEN "Predominantly Makeshift"
 			  WHEN roof = 7 THEN "N/A"		
-		END) as roof_mat
+		END)
 FROM db_hpq.hpq_hh;
+
+INSERT INTO household
+SELECT H.id, L.id AS location, house_type, wall, roof
+FROM db_hpq.hpq_hh H LEFT JOIN location L 
+		ON H.mun = L.mun AND H.zone = L.zone AND H.brgy = L.brgy AND H.purok = L.purok
+GROUP BY H.id;
+
+INSERT INTO crop(volume, line, typeID, householdID)
+SELECT crop_vol, crop_line, IFNULL(croptype, 6), h.id 
+FROM db_hpq.hpq_crop c INNER JOIN household h ON( c.hpq_hh_id = h.id);
+
+INSERT INTO aquani(volume, line, typeID, householdID)
+SELECT distinct aquani_vol, aquani_line, IFNULL(aquanitype, 7), h.id 
+FROM db_hpq.hpq_aquani a INNER JOIN household h ON ( a.hpq_hh_id = h.id);
